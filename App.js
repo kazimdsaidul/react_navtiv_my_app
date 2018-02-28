c4f36fef22e1e201c91014b94b59c9c943f916e0
Kazi Md. Saddul <kazimdsaidul@gmail.com>
2018-02-28 16:36:08 +0600

    Day end commit 28-02-2018

--- App.js
+++ App.js
@@ -1,122 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
 
-} from 'react-native';
-
-import  ListItem from './src/components/listItem/ListItem';
-
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-
-  // status
+export default class App extends Component {
   state = {
-    placeName: "",
-    placeData: []
-  }
+    places: []
+  };
 
-  // on change hander 
-  placeNameChangeHander = value => {
-
-
-    this.setState({
-      placeName: value
-      
+  placeAddedHandler = placeName => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(placeName)
+      };
     });
   };
 
-  // hander on button press 
-  placeButtonPressHander = () => {
-  
-
-    if (this.state.placeName.trim === "") {
-      return;
-    }
-
-    
-    this.setState(preState =>{
-      return{
-        placeData : preState.placeData.concat(preState.placeName)
-      }
-    });
-    this.setState({
-      placeName: ""
-    });
-  };
-
-   
   render() {
-    
-    const placeOutput = this.state.placeData.map((place, i) => (
-      <ListItem key = {i} placeName= {place} ></ListItem>  
-    
-    ));
-
-
     return (
       <View style={styles.container}>
-        <View style={styles.inputContainer} >
-          <TextInput style = {styles.inputText}
-            value={this.state.placeName}
-            onChangeText={this.placeNameChangeHander}
-          />
-          <Button 
-            style = {styles.button}
-            title="Add"
-            color="#841584"
-            onPress ={this.placeButtonPressHander}
-          />
-        </View>    
-       <View style = {styles.listContainer} >{placeOutput}</View>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList places={this.state.places} />
       </View>
     );
   }
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    backgroundColor: '#F5FCFF',
-  }, 
-  inputContainer: {
-    marginTop: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  
-  inputText: {
-    width : "70%"
-  },
-
-  button: {
-    width : "30%"
-  },
-  listContainer: {
-    width : "100%"
+    padding: 26,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
   }
-
-
 });
--- src/components/listItem/ListItem.js
+++ src/components/listItem/ListItem.js
@@ -1,22 +1,23 @@
-// this custom list item
 import React from 'react';
-import { View, Text , StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-const ListItem = (props) => (
-    <View style = {styles.listItem} > 
-        <Text> {props.placeName}</Text>
-    </View>
+const listItem = (props) => (
+
+    <TouchableOpacity onPress={props.onItemPressed} >
+        <View style={styles.listItem}>
+            <Text>{props.placeName}</Text>
+        </View>
+    </TouchableOpacity>
 
 );
 
-
 const styles = StyleSheet.create({
-    listItem:{
-        width : "100%",
+    listItem: {
+        width: "100%",
+        marginBottom: 5,
         padding: 10,
-        backgroundColor: "#eeee",
+        backgroundColor: "#eee"
     }
-
 });
 
-export default ListItem;
+export default listItem;
