55a747f10b320223ac2f1011528a69b6dcf0c24c
Kazi Md. Saddul <kazimdsaidul@gmail.com>
2018-03-01 09:36:34 +0600

    Done FlatList

--- App.js
+++ App.js
@@ -12,29 +12,32 @@ export default class App extends Component {
   placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(placeName)
+        places: prevState.places.concat({
+          key: Math.random(),
+          value: placeName
+        })
       };
     });
   };
 
-  placeDeletedHander = index =>{
-    this.setState(prevState =>{
+  placeDeletedHandler = key => {
+    this.setState(prevState => {
       return {
-        places : prevState.places.filter( (place , i)=>{
-           return i !== index;
+        places: prevState.places.filter(place => {
+          return place.key !== key;
         })
-      }
+      };
     });
-
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceInput onPlaceAdded = {this.placeAddedHandler} />
         <PlaceList
-         places={this.state.places}  
-         onItemDeleted = {this.placeDeletedHander} />
+          places={this.state.places}
+          onItemDeleted={this.placeDeletedHandler}
+        />
       </View>
     );
   }
