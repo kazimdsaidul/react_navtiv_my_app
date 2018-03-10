1a442d50b5ae80ed778790bd718c6bc344004398
Kazi Md. Saddul <kazimdsaidul@gmail.com>
2018-03-10 12:28:22 +0600

    Day end commit 09-03-2018

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 import { View, TextInput, Button, StyleSheet } from "react-native";
 
-class PlaceInput extends Component {
-  state = {
-    placeName: ""
-  };
 
   componentDidMount() {
     
@@ -40,23 +36,5 @@ class PlaceInput extends Component {
         />
       </View>
     );
-  }
-}
-
-const styles = StyleSheet.create({
-  inputContainer: {
-    // flex: 1,
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  placeInput: {
-    width: "70%"
-  },
-  placeButton: {
-    width: "30%"
-  }
-});
 
 export default PlaceInput;
